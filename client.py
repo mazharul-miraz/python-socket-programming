557e4982b13c8e2451215e5f15cdf2c688f35b9f
mazharul.miraz <mazharul.miraz@outlook.com>
2019-11-08 01:22:21 +0530

    client receving method updated

--- client.py
+++ client.py
@@ -1,5 +1,12 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1024))
-msg =s.recv(1024)
-print(msg.decode("utf-8"))
+s.connect((socket.gethostname(), 1025))
+
+complete_info = ''
+while True:
+    msg=s.recv(7)
+    if len(msg) <=0:
+        break
+    complete_info += msg.decode("utf-8")
+
+print(complete_info)
--- server.py
+++ server.py
@@ -1,11 +1,10 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((socket.gethostname(), 1024))
+s.bind((socket.gethostname(), 1025))
 s.listen(5)
-
-
 
 while True:
     clt, adr=s.accept()
     print(f"Connection to {adr} established ")
     clt.send(bytes("Wellcome to socket programming in python", "utf-8" ))
+    clt.close()
